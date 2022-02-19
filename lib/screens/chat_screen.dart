b91dab0e5c2d6cf836e31efe80753f5f1ca32bd1
Jeremiah Jacob <jaykon4me@gmail.com>
2022-02-19 10:56:36 +0100

    created message bubble

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:start_chat/models/message_data.dart';
+import 'package:start_chat/theme.dart';
 import 'package:start_chat/widgets/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -54,7 +55,193 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Text('text'),
+      body: _DemoMessageList(),
+    );
+  }
+}
+
+class _DemoMessageList extends StatelessWidget {
+  const _DemoMessageList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView(
+        children: const [
+          _DateLabel(label: 'Yesterday'),
+          _MessageTile(
+            message: 'Hi, Lucy! How\'s your day going?',
+            messageDate: '12:01 PM',
+          ),
+          _MessageOwnTile(
+            message: 'You know how it goes...',
+            messageDate: '12:02 PM',
+          ),
+          _MessageTile(
+            message: 'Do you want Starbucks?',
+            messageDate: '12:02 PM',
+          ),
+          _MessageOwnTile(
+            message: 'Would be awesome!',
+            messageDate: '12:03 PM',
+          ),
+          _MessageTile(
+            message: 'Coming up!',
+            messageDate: '12:03 PM',
+          ),
+          _MessageOwnTile(
+            message: 'YAY!!!',
+            messageDate: '12:03 PM',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MessageTile extends StatelessWidget {
+  const _MessageTile({
+    Key? key,
+    required this.message,
+    required this.messageDate,
+  }) : super(key: key);
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(_borderRadius),
+                  topRight: Radius.circular(_borderRadius),
+                  bottomRight: Radius.circular(_borderRadius),
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Text(message),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MessageOwnTile extends StatelessWidget {
+  const _MessageOwnTile({
+    Key? key,
+    required this.message,
+    required this.messageDate,
+  }) : super(key: key);
+
+  final String message;
+  final String messageDate;
+
+  static const _borderRadius = 26.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: AppColors.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(_borderRadius),
+                  bottomRight: Radius.circular(_borderRadius),
+                  bottomLeft: Radius.circular(_borderRadius),
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
+                child: Text(message,
+                    style: const TextStyle(
+                      color: AppColors.textLigth,
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                messageDate,
+                style: const TextStyle(
+                  color: AppColors.textFaded,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DateLabel extends StatelessWidget {
+  const _DateLabel({
+    Key? key,
+    required this.label,
+  }) : super(key: key);
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+            child: Text(
+              label,
+              style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textFaded),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
